5148cb788e767c9170bcb7e2d63c2046727ec55f
Aarya Patel <aarya.patel@gmail.com>
2025-04-02 18:04:31 -0400

    Added zathura theme

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -20,8 +20,15 @@
   home.packages = with pkgs; [
     # LaTeX Deps
     tectonic
-    zathura
   ];
+
+  programs.zathura = {
+    enable = true;
+    extraConfig = builtins.readFile (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/dracula/zathura/master/zathurarc";
+      sha256 = "03r5gbfp3m4hlzba61lgk0z95avjjlb9h8jb1nacv44aqcr04s84"; # Replace with actual hash
+    });
+  };
 
   xdg = {
     enable = true;
