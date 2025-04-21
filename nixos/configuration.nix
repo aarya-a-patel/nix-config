61040d113a478b470b44d78478756dd1a13d2164
Aarya Patel <aarya.patel@gmail.com>
2025-04-21 17:32:38 -0400

    Enabled nix ld

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -280,6 +280,8 @@
   };
   programs.virt-manager.enable = true;
 
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
