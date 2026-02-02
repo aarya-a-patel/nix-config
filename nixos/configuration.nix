5eca0c5a29bed11008018def4fba241a5bd3cebb
Aarya Patel <aarya.patel@gmail.com>
2026-02-02 14:21:04 -0500

    Added auto upgrade

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -96,20 +96,15 @@
   };
 
   # Auto upgrade
-  /*
   system.autoUpgrade = {
     enable = true;
     flake = inputs.self.outPath;
     flags = [
-      "--update-input"
-      "nixpkgs"
-      "--no-write-lock-file"
       "-L" # print build logs
     ];
     dates = "02:00";
     randomizedDelaySec = "45min";
   };
-  */
 
   # System clock is local time
   time.hardwareClockInLocalTime = true;
