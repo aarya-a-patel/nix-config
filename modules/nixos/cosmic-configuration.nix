9fc1ea9c8cbac46509cc595b203d3890a33a7018
Aarya Patel <aarya.patel@gmail.com>
2026-02-09 19:22:11 -0500

    Added cosmic applets

--- modules/nixos/cosmic-configuration.nix
+++ modules/nixos/cosmic-configuration.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./xdg-configuration.nix
   ];
@@ -6,4 +6,10 @@
   # Enable the COSMIC Desktop Evironment.
   # services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    cosmic-ext-applet-privacy-indicator
+    cosmic-ext-applet-caffeine
+    cosmic-ext-tweaks
+  ];
 }
