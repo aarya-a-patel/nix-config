3aabce1b4c8063d8df6e571ba5667228af8cc20c
Aarya Patel <aarya.patel@gmail.com>
2026-03-11 17:08:00 -0400

    Kernel optimizations

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -23,10 +23,24 @@
     };
     initrd.verbose = false;
     consoleLogLevel = 0;
-    kernelParams = ["quiet" "udev.log_level=0"];
-    kernelPackages = pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest-x86_64-v4;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=0"
+      "transparent_hugepage=madvise"
+    ];
+    kernelPackages = let
+      helpers = pkgs.callPackage "${inputs.nix-cachyos-kernel.outPath}/helpers.nix" {};
+    in
+      helpers.kernelModuleLLVMOverride (pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest-lto-x86_64-v4);
 
     # Enable NTFS
     supportedFilesystems.ntfs = true;
+
+    kernel.sysctl = {
+      # reduce disk swapping
+      "vm.swappiness" = 10;
+      # keep filesystem caches longer
+      "vm.vfs_cache_pressure" = 50;
+    };
   };
 }
